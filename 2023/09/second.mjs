7add96a27289eb143455703099e49ad92d8ba0e8
Aleksi Seppänen <aleksi@seppanen.dev>
2023-12-09 17:02:09 +0200

    2023 09

---
+++ 2023/09/second.mjs
@@ -0,0 +1,35 @@
+import { readFile, p } from '../common.mjs';
+
+const lines = await readFile('input.txt');
+
+let answer = 0
+
+const datasetRecursive = (dataset) => {
+    const latest = dataset[dataset.length - 1]
+    const nextDataset = []
+    for ( let i = 0; i < latest.length - 1; i++ ) {
+        nextDataset.push(latest[i+1] - latest[i])
+    }
+    dataset.push(nextDataset)
+
+    if (nextDataset.every(v => v === 0)) {
+        return dataset
+    }
+    return datasetRecursive(dataset)
+}
+
+for (const line of lines) {
+    const dataset = line.split(' ').map(n => parseInt(n));
+
+    const sequence = datasetRecursive([dataset])
+
+    let a = 0
+    let last = 0
+    for (let i = sequence.length - 1; i >= 0; i--) {
+        const first = sequence[i][0]
+        a = first - last
+        last = a
+    }
+    answer = answer + a
+}
+p(answer)
--- 2023/starter.mjs
+++ 2023/starter.mjs
@@ -1,6 +1,6 @@
 import { readFile, p } from '../common.mjs';
 
-const lines = await readFile('input.txt');
+const lines = await readFile('example.txt');
 
 for (const line of lines) {
     p(line);
